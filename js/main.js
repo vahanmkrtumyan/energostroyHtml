4ec01f27688fe29a1668016678d60eb083214234
AshotSimonyan <a.simonyan.stdev@gmail.com>
2020-08-18 22:43:28 +0400

    Init

---
+++ js/main.js
@@ -0,0 +1,31 @@
+scrollTo = (element) => {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: element.offsetTop
+  });
+
+}
+document.getElementById("about").addEventListener('click', () => {
+  scrollTo(document.getElementById("aboutNew"));
+});
+
+document.getElementById("service").addEventListener('click', () => {
+  scrollTo(document.getElementById("serviceNew"));
+});
+
+document.getElementById("contactUs").addEventListener('click', () => {
+  scrollTo(document.getElementById("contactUsNew"));
+});
+
+// var backToTop = document.getElementById("back-top");
+
+
+// window.addEventListener('scroll', function() {
+//  var Y = window.scrollY;
+//  if (Y > 500) {
+//   backToTop.classList.add("active");
+//  } else {
+//   backToTop.classList.remove("active");
+//  }
+// });
