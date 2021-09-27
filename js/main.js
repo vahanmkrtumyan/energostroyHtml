bc5d9650f233278d27f7ee61fd9376fc7f0d8cb6
vahanmkrtumyan <89935857+vahanmkrtumyan90@users.noreply.github.com>
2021-09-27 20:43:06 +0400

    changes

--- js/main.js
+++ js/main.js
@@ -22,6 +22,26 @@ document.getElementById("contactUs").addEventListener("click", () => {
   scrollTo(document.getElementById("contactUsNew"));
 });
 
+document.getElementById("header1").addEventListener("click", () => {
+  scrollTo(document.getElementById("headerNew"));
+  $(".responsive-mobile-menu").removeClass("active");
+});
+
+document.getElementById("about1").addEventListener("click", () => {
+  scrollTo(document.getElementById("aboutNew"));
+  $(".responsive-mobile-menu").removeClass("active");
+});
+
+document.getElementById("service1").addEventListener("click", () => {
+  scrollTo(document.getElementById("serviceNew"));
+  $(".responsive-mobile-menu").removeClass("active");
+});
+
+document.getElementById("contactUs1").addEventListener("click", () => {
+  scrollTo(document.getElementById("contactUsNew"));
+  $(".responsive-mobile-menu").removeClass("active");
+});
+
 const services = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
 ];
